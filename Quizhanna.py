49a71c6d460dbc293e3545ee1b1a8f56ca7b4dd2
HannamLahtinen <hanna.lahtinen@skillio.ai>
2024-10-21 16:32:32 +0300

    "Feedback completed"

--- Quizhanna.py
+++ Quizhanna.py
@@ -2,30 +2,46 @@ counter = 0
 answers = 0
 
 while True:
-    wannaplay = input("Do you want to start quiz 2 (yes/no)")
+    wannaplay = input("Do you want to start quiz 2 (yes/no) ")
     if wannaplay == "no":
-        print("Goodbye")
+        print("Ok, maybe next time.")
+        print("Goodbye!")
         break
     elif wannaplay == "yes":
-        q1 = input("What is the capital of Italy?")
+        q1 = input("What is the capital of Italy? ")
         if q1 == "Rome":
             counter += 1
+            print("Correct!")
+        else:
+            print("Incorrect answer")
         answers += 1
-        q2 = input("What is the capital of India?")
+        q2 = input("What is the capital of India? ")
         if q2 == "New Delhi":
             counter += 1
+            print("Correct!")
+        else:
+            print("Incorrect answer")
         answers += 1
-        q3 = input("What is the capital of Uruguay?")
+        q3 = input("What is the capital of Uruguay? ")
         if q3 == "Montevideo":
             counter += 1
+            print("Correct!")
+        else:
+            print("Incorrect answer")
         answers += 1
-        q4 = input("What is the capital of Ghana?")
+        q4 = input("What is the capital of Ghana? ")
         if q4 == "Accra":
             counter +=1
+            print("Correct!")
+        else:
+            print("Incorrect answer")
         answers += 1
-        q5 = input("Last one: what is the capital of Micronesia?")
+        q5 = input("Last one: what is the capital of Micronesia? ")
         if q5 == "Palikir":
             counter += 1
+            print("Correct!")
+        else:
+            print("Incorrect answer")
         answers += 1
     if answers == 5:
         print(f"You got {counter}/5 points")
