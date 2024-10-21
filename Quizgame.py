3ca74aa735e2fb9ea7a6221a9cda1b08618632d0
”MiikaMustamaki” <miika.mustamaki@skillio.ai>
2024-10-21 15:24:26 +0300

    created simple quiz game

---
+++ Quizgame.py
@@ -0,0 +1,36 @@
+def ask_questions():
+    score = 0
+
+    questions = [
+        {"question": "What year Finland won its first gold in ice hockey world championship", "answer": "1995"},
+        {"question": "What is the chemical symbol for water?", "answer": "H2O"},
+        {"question": "What year did the Titanic sink?", "answer": "1912"},
+        {"question": "What is the capital of France?", "answer": "Paris"}
+    ]
+
+    for q in questions:
+        print(q["question"])
+        answer = input("Answer: ")
+
+        if answer.lower() == q["answer"].lower():
+            print("Correct!!")
+            score +=1
+        else:
+            print("Wrong :(")
+
+    return score, len(questions)
+
+def quiz_game():
+    while True:
+        start = input("Start Quiz Game? (yes/no) ")
+
+        if start == "yes":
+            print("Game starting..\n")
+            score, total = ask_questions()
+            print(f"You got {score}/{total} correct!")
+        else:
+            print("Sad, goodbye")
+            break
+
+#start
+quiz_game()
